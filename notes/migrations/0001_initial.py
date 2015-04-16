c1de230b7f47f1a872b872188c73401e6c282d3a
HugoCapitan <hmoravilez@gmail.com>
2015-04-16 10:41:12 -0500

    Notes functionality, create note, detailview and listview

---
+++ notes/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Note',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=100)),
+                ('content', models.TextField()),
+                ('date', models.DateField(auto_now_add=True)),
+            ],
+            options={
+                'verbose_name': 'Note',
+                'verbose_name_plural': 'Notes',
+            },
+        ),
+    ]
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import CreateView, ListView, DetailView
+
+from .models import Note
+
+from .forms import note_creation_form
+
+class note_createview(CreateView):
+	model = Note
+	form_class  = note_creation_form
+	success_url = '/notes/'
+	template_name = 'create_note.html'
+
+class note_listview(ListView):
+	model = Note
+	template_name = 'notes.html'
+
+class note_detailview(DetailView):
+	model = Note
+	template_name = 'note_detail.html'
